7c58f1401e2c7b9a0e0e39ca310468ab1aba0b62
Marish <marishalagiri@gmail.com>
2025-06-29 12:49:02 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
+// app.use(cors({
+//   origin: "http://localhost:5173", // or your frontend URL
+//   credentials: true
+// }));
 app.use(cors({
-  origin: "http://localhost:5173", // or your frontend URL
+  origin: "https://jd-zum.vercel.app", // or your frontend URL
   credentials: true
 }));
 app.use(express.json());
