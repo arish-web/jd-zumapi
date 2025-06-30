4a6acbdef41ed4799fc3ff3f1fc488933178bbfc
Marish <marishalagiri@gmail.com>
2025-06-30 19:23:55 +0530

    Tattoo price field data type issue fixed

--- models/TattooModel.js
+++ models/TattooModel.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
 
-const TattooSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  artist: { type: String, required: true }, // filled from req.user
-  style: { type: String, required: true },
-  category: { type: String, required: true },
-  date: { type: String, required: true },
-  price: { type: String, required: true },
-  uniqueCode: { type: String, required: true, unique: true },
-  image: { type: String }, // you can also use Buffer if storing as file
-  ownerId: { type: String, required: true},
-  deleted: { type: Boolean, default: false },
-}, { timestamps: true });
+const TattooSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    artist: { type: String, required: true }, // filled from req.user
+    style: { type: String, required: true },
+    category: { type: String, required: true },
+    date: { type: String, required: true },
+    price: { type: Number, required: true },
+    uniqueCode: { type: String, required: true, unique: true },
+    image: { type: String }, // you can also use Buffer if storing as file
+    ownerId: { type: String, required: true },
+    deleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 const Tattoo = mongoose.model("tattoo", TattooSchema);
 export default Tattoo;
-
