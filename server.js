a1d73b605a5b62c366a8c66c052b4a084314bff2
Marish <marishalagiri@gmail.com>
2025-06-30 19:44:33 +0530

    photo payload size increased due to 2.1mb cannot update in add photography

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.use(express.json());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use('/api', authRoutes);
 app.use('/api', tattooRoutes);
